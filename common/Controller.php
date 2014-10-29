ee09bc8eec14f20dc14c2d13f1bb0eb3558c5733
dangozero <dangozero@gmail.com>
2014-10-29 13:55:29 +0300

    Fix common Controller

--- common/Controller.php
+++ common/Controller.php
@@ -5,6 +5,7 @@
  * @license LICENSE
  */
 namespace webnula2\common;
+
 use webnula2\components\UrlRule;
 
 
@@ -12,7 +13,8 @@ use webnula2\components\UrlRule;
  * Class Controller
  * @package webnula2\common
  */
-class Controller extends \CController {
+class Controller extends \CController
+{
 	/**
 	 * @var
 	 */
@@ -25,36 +27,34 @@ class Controller extends \CController {
 	 * @var \CAttributeCollection
 	 */
 	public $params;
-
 	/**
 	 * @var array
 	 */
 	private $rules = array();
-
 	/**
 	 * @var string
 	 */
 	public $assetsUrl = '';
-
 	/**
 	 * @var \CClientScript
 	 */
 	public $cs;
-
 	/**
 	 * @var string
 	 */
 	private $_pageTitle;
-
 	/**
 	 * @var array
 	 */
 	private $_titles = array();
-
 	/**
 	 * @var array
 	 */
-	private $_breadcrumbs= array();
+	private $_breadcrumbs = array();
+	/**
+	 * @var bool
+	 */
+	public $exclude = true;
 
 	/**
 	 *
@@ -66,19 +66,22 @@ class Controller extends \CController {
 
 		$this->cs = \Yii::app()->getClientScript();
 
-		if( ($assetsPath = \Yii::getPathOfAlias('application.assets')) && is_dir($assetsPath)) {
-			$this->assetsUrl = ( YII_DEBUG ?
-				\Yii::app()->getAssetManager()->publish( $assetsPath, false, -1, true ) :
-				\Yii::app()->getAssetManager()->publish( $assetsPath )
-			);
+		if ( ( $assetsPath = \Yii::getPathOfAlias( 'application.assets' ) ) && is_dir( $assetsPath ) ) {
+			$this->assetsUrl = ( YII_DEBUG ? \Yii::app()->getAssetManager()->publish( $assetsPath, false, -1, true ) : \Yii::app()->getAssetManager()->publish( $assetsPath ) );
 		}
 
-		if(isset($this->section)) {
-			foreach( $this->section->cache(1000)->getParents('publish=1') as $parent ) {
-				$this->_breadcrumbs[] = array($parent->title, $parent->url);
+		if ( isset( $this->section ) ) {
+			foreach ( $this->section->cache( 1000 )->getParents( 'publish=1' ) as $parent ) {
+				$this->_breadcrumbs[] = array( $parent->title, $parent->url );
 			}
-			$this->_breadcrumbs[] = array($this->section->title, $this->section->url);
+			$this->_breadcrumbs[] = array( $this->section->title, $this->section->url );
+		} else if ( !empty( \Yii::app()->params->projectName ) ) {
+			$this->_breadcrumbs[] = array( \Yii::app()->params->projectName, '/' );
 		}
+
+		$this->attachBehavior('global', array(
+			'class' => 'application\components\GlobalBehavior'
+		));
 	}
 
 	/**
@@ -86,12 +89,12 @@ class Controller extends \CController {
 	 *
 	 * @return bool
 	 */
-	public function __isset($name)
+	public function __isset( $name )
 	{
-		if( isset($this->params[$name])) {
+		if ( isset( $this->params[$name] ) ) {
 			return true;
 		} else {
-			return parent::__isset($name);
+			return parent::__isset( $name );
 		}
 	}
 
@@ -100,41 +103,45 @@ class Controller extends \CController {
 	 *
 	 * @return mixed
 	 */
-	public function __get($name)
+	public function __get( $name )
 	{
-		if(isset($this->params[$name])) {
+		if ( isset( $this->params[$name] ) ) {
 			return $this->params[$name];
 		}
-		return parent::__get($name);
+
+		return parent::__get( $name );
 	}
 
 	/**
 	 * @param string $cssFile
 	 */
-	public function registerCssFile($cssFile) {
-		$this->cs->registerCssFile($this->assetsUrl . '/' .$cssFile);
+	public function registerCssFile( $cssFile )
+	{
+		$this->cs->registerCssFile( $this->assetsUrl . '/' . $cssFile );
 	}
 
 	/**
 	 * @param string $scriptFile
 	 */
-	public function registerScriptFile($scriptFile) {
-		$this->cs->registerScriptFile($this->assetsUrl . '/' .$scriptFile);
+	public function registerScriptFile( $scriptFile )
+	{
+		$this->cs->registerScriptFile( $this->assetsUrl . '/' . $scriptFile );
 	}
 
 	/**
 	 * @param string $coreName
 	 */
-	public function registerCoreScript($coreName) {
-		$this->cs->registerCoreScript($coreName);
+	public function registerCoreScript( $coreName )
+	{
+		$this->cs->registerCoreScript( $coreName );
 	}
 
 	/**
 	 * @param string $name
 	 */
-	public function registerPackage($name)
+	public function registerPackage( $name )
 	{
-		$this->cs->registerPackage($name);
+		$this->cs->registerPackage( $name );
 	}
 
 	/**
@@ -143,8 +150,9 @@ class Controller extends \CController {
 	 * @param string $position
 	 * @param array $htmlOptions
 	 */
-	public function registerScript($id, $script, $position = null, $htmlOptions = array()) {
-		switch( $position ) {
+	public function registerScript( $id, $script, $position = null, $htmlOptions = array() )
+	{
+		switch ( $position ) {
 			case 'load':
 				$position = \CClientScript::POS_LOAD;
 				break;
@@ -162,7 +170,7 @@ class Controller extends \CController {
 				$position = \CClientScript::POS_READY;
 				break;
 		}
-		$this->cs->registerScript($id, $script, $position, $htmlOptions);
+		$this->cs->registerScript( $id, $script, $position, $htmlOptions );
 	}
 
 	/**
@@ -170,9 +178,9 @@ class Controller extends \CController {
 	 * @param string $css
 	 * @param string $media
 	 */
-	public function registerCss($id, $css, $media = '')
+	public function registerCss( $id, $css, $media = '' )
 	{
-		$this->cs->registerCss($id, $css, $media);
+		$this->cs->registerCss( $id, $css, $media );
 	}
 
 	/**
@@ -194,10 +202,10 @@ class Controller extends \CController {
 	/**
 	 * @param $item
 	 */
-	public function addLinkItem($item)
+	public function addLinkItem( $item )
 	{
-		if( !is_array($item) ) {
-			$item = array($item);
+		if ( !is_array( $item ) ) {
+			$item = array( $item );
 		}
 		$this->_breadcrumbs[] = $item;
 	}
@@ -215,13 +223,14 @@ class Controller extends \CController {
 	 */
 	public function getPageTitle()
 	{
-		if($this->_pageTitle === null) {
-			foreach( $this->_breadcrumbs as $linkItem ) {
+		if ( $this->_pageTitle === null ) {
+			foreach ( $this->_breadcrumbs as $linkItem ) {
 				$this->_titles[] = $linkItem[0];
 			}
 
-			$this->_pageTitle = implode(' :: ', array_reverse($this->_titles));
+			$this->_pageTitle = implode( ' :: ', array_reverse( $this->_titles ) );
 		}
+
 		return $this->_pageTitle;
 	}
 
@@ -232,76 +241,148 @@ class Controller extends \CController {
 	private function processRules()
 	{
 		$rules = $this->route();
-		if( $cache = \Yii::app()->getComponent('cache') )
-		{
-			$hash=md5(serialize($rules));
-			if( ($data=$cache->get(get_class($this))) !== false && isset($data[1]) && $data[1]===$hash )
-			{
-				$this->rules=$data[0];
+		if ( $cache = \Yii::app()->getComponent( 'cache' ) ) {
+			$hash = md5( serialize( $rules ) );
+			if ( ( $data = $cache->get( get_class( $this ) ) ) !== false && isset( $data[1] ) && $data[1] === $hash ) {
+				$this->rules = $data[0];
+
 				return;
 			}
 		}
 
-		if( !empty($rules) ) {
-			if( is_array($rules) ) {
-				foreach( $rules as $pattern => $route ) {
-					$this->rules[] = new UrlRule($route, $pattern);
+		if ( !empty( $rules ) ) {
+			if ( is_array( $rules ) ) {
+				foreach ( $rules as $pattern => $route ) {
+					$this->rules[] = new UrlRule( $route, $pattern );
 				}
-			} else if( is_string($rules) ) {
+			} else if ( is_string( $rules ) ) {
 				$this->rules = $rules;
 			}
 		}
-		if(isset($cache))
-			$cache->set(get_class($this),array($this->rules,$hash));
+		if ( isset( $cache ) ) {
+			$cache->set( get_class( $this ), array( $this->rules, $hash ) );
+		}
 	}
 
 	/**
 	 * Run the controller action.
 	 */
-	public function run($actionID)
+	public function run( $actionID )
 	{
 		$rawPathInfo = $this->params['routeInfo'];
 
-		if( !empty($this->rules) ) {
-			if( is_string($this->rules) )
-				$actionID = $this->rules;
-			else {
-				$manager = \Yii::app()->getUrlManager();
-				$request = \Yii::app()->getRequest();
-				$pathInfo = trim($rawPathInfo, '/');
-				foreach($this->rules as $rule) {
-					if(($actionID = $rule->parseUrl($manager, $request, $pathInfo, $rawPathInfo))) {
-						$this->params->mergeWith($_GET);
-						break;
-					}
+		$beforeActionId = $actionID;
+		if ( false === $actionID = $this->processRoute( $rawPathInfo ) ) {
+			$actionID = $beforeActionId;
+			if ( mb_strrpos( $rawPathInfo, '/' ) !== false ) {
+				$action = (string)mb_substr( $rawPathInfo, 1, mb_strpos( $rawPathInfo, '/', 1 ) - 1 );
+				if ( !empty( $action ) && $actionID != 'error' ) {
+					$actionID = $action;
 				}
 			}
 		}
 
-		if( $actionID === false && trim($rawPathInfo, '/') !== '' ) {
-			$action = (string)mb_substr( $rawPathInfo, 1, mb_strpos( $rawPathInfo, '/', 1 ) - 1 );
-			if ( !empty( $action ) && $actionID != 'error' ) {
-				$actionID = $action;
+		if(($action=$this->createAction($actionID))!==null)
+		{
+			if( $this->beforeAction($action) )
+			{
+				$this->runActionWithFilters($action,$this->filters());
+				$this->afterAction($action);
 			}
-		}
-
-		// run action
-		if(($action=$this->createAction($actionID))!==null) {
-			$this->runActionWithFilters($action,$this->filters());
 		} else {
 			$this->missingAction( $actionID );
 		}
 	}
 
 	/**
+	 * @param \CAction $action
+	 *
+	 * @return bool
+	 */
+	public function beforeAction($action) {
+		$this->onBeforeAction(new \CEvent($this, array($action)));
+
+		if(($parent=$this->getModule())===null)
+			$parent=Yii::app();
+
+		return $parent->beforeControllerAction($this,$action);
+	}
+
+	/**
+	 * @param \CEvent $event
+	 *
+	 * @throws \CException
+	 */
+	public function onBeforeAction( \CEvent $event ) {
+		$this->raiseEvent('onBeforeAction', $event);
+	}
+
+	/**
+	 * @param \CAction $action
+	 */
+	public function afterAction($action) {
+		$this->onAfterAction(new \CEvent($this, array($action)));
+
+		if(($parent=$this->getModule())===null)
+			$parent=Yii::app();
+		$parent->afterControllerAction($this,$action);
+	}
+
+	/**
+	 * @param \CEvent $event
+	 *
+	 * @throws \CException
+	 */
+	public function onAfterAction( \CEvent $event ) {
+		$this->raiseEvent('onAfterAction', $event);
+	}
+
+	/**
+	 * @param string|array $tpl_var
+	 * @param mixed $value
+	 */
+	public function assign( $tpl_var, $value = null )
+	{
+		\Yii::app()->viewRenderer->assign( $tpl_var, $value );
+	}
+
+	/**
+	 * @param $rawPathInfo
+	 *
+	 * @return bool
+	 */
+	private function processRoute( $rawPathInfo )
+	{
+		if ( !empty( $this->rules ) ) {
+			if ( is_string( $this->rules ) ) {
+				return $this->rules;
+			} else {
+				$manager = \Yii::app()->getUrlManager();
+				$request = \Yii::app()->getRequest();
+				$pathInfo = trim( $rawPathInfo, '/' );
+				foreach ( $this->rules as $rule ) {
+					if ( ( $r = $rule->parseUrl( $manager, $request, $pathInfo, $rawPathInfo ) ) ) {
+						$this->params->mergeWith( $_GET );
+
+						return $r;
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
 	 * @param $output
 	 */
-	public function json($output) {
-		header('Cache-Control: no-cache, must-revalidate');
-		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-		header('Content-type: application/json');
+	public function json( $output )
+	{
+		header( 'Cache-Control: no-cache, must-revalidate' );
+		header( 'Expires: Mon, 26 Jul 1997 05:00:00 GMT' );
+		header( 'Content-type: application/json' );
 
-		echo \CJSON::encode($output);
+		echo \CJSON::encode( $output );
 		\Yii::app()->end();
 	}
 
@@ -310,6 +391,6 @@ class Controller extends \CController {
 	 */
 	public function getCacheKey()
 	{
-		return get_class($this);
+		return get_class( $this );
 	}
 }
